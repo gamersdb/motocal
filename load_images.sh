d867f4dd76724b581504ffd6eb9528f6125a8128
gamersdb <dummy@gamersdb.info>
2020-04-29 18:50:18 +0900

    toriaezu

---
+++ load_images.sh
@@ -0,0 +1,2 @@
+gsutil cp `gsutil ls | grep images`imgs.tgz .
+gsutil cp `gsutil ls | grep images`charaimgs.tgz .
---
+++ save_images.sh
@@ -0,0 +1,2 @@
+gsutil cp imgs.tgz `gsutil ls | grep images`
+gsutil cp charaimgs.tgz `gsutil ls | grep images`
